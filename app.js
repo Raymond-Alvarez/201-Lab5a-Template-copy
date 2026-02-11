10b04f6e795d4bbcedc51f16e7e21d3f4451cbb5
raymond alvarez <raymond.alvarez1127@gmail.com>
2026-02-11 11:13:22 -0800

    update stretch goal

--- app.js
+++ app.js
@@ -154,10 +154,8 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
     return [totalProduct, productString];
 }
 
-// Uncomment to test
-testMultiplyAnyArray(testDynamicArray);
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
